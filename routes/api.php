f5221e44d63d7fb04f5db8e69a450ca90f960404
czw <549226266@qq.com>
2017-04-10 17:15:36 +0800

    init

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Http\Request;
 
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
-    $api->group(['middleware' => 'auth:api'], function ($api) {
-        
+    $api->group(['middleware' => 'auth:api', 'namespace' => 'App\Http\Controllers\Api\V1'], function ($api) {
+        $api->resource('user', 'UserController');
     });
 });
